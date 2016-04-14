2d52bd686aec6d9b42469100a79b8fa07efe8dd1
Kojitsari <kojitsari@gmail.com>
2016-04-14 05:11:26 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TwitchChatSharp;
+
+namespace PhantomBot 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TwitchConnection(
+                cluster: ChatEdgeCluster.Aws,
+                nick: "mybotsname",
+                oauth: "592348324093290489802993482042", // do not include the oauth: prefix
+                port: 6697,
+                capRequests: new string[] { "twitch.tv/tags", "twitch.tv/commands" },
+                ratelimit: 1500,
+                secure: true
+                );
+
+            client.Connected += (object sender, IrcConnectedEventArgs e) =>
+            {
+                Console.WriteLine("Connected");
+                client.JoinChannel("#mychannelname"); // include the #, in the same format you would for an IRC client
+            };
+
+            client.MessageReceived += (object sender, IrcMessageEventArgs e) =>
+            {
+                Console.WriteLine("Received: " + e.Message.ToString());
+            };
+
+            client.Reconnected += (object sender, EventArgs e) =>
+            {
+                Console.WriteLine("Reconnected");
+            };
+
+            client.Connect();
+        }
+    }
+}
